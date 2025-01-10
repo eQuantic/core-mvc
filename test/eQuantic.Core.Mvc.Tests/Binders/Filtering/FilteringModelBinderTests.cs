c25fee73bb279c72fd7ef53300220ea8f6a61efe
Edgar Mesquita <edgar.rj@gmail.com>
2025-01-10 14:24:31 +0000

    fix: MVC reference

--- test/eQuantic.Core.Mvc.Tests/Binders/Filtering/FilteringModelBinderTests.cs
+++ test/eQuantic.Core.Mvc.Tests/Binders/Filtering/FilteringModelBinderTests.cs
@@ -1,10 +1,8 @@
 ﻿using System.Globalization;
 using eQuantic.Core.Mvc.Binders.Filtering;
 using eQuantic.Linq.Filter;
-using Microsoft.AspNetCore.Http.Internal;
-using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.Extensions.Primitives;
 
 namespace eQuantic.Core.Mvc.Tests.Binders.Filtering;
@@ -16,26 +14,28 @@ public class FilteringModelBinderTests
     public async Task FilteringModelBinder_BindModelAsync_successfully()
     {
         var binder = new FilteringModelBinder();
-        var compositeMetadataDetailsProvider =
-            new DefaultCompositeMetadataDetailsProvider(new []{ new DefaultBindingMetadataProvider()});
+        var valueProvider = new QueryStringValueProvider(
+            new BindingSource(Guid.NewGuid().ToString(), "filterBy", false, true),
+            new QueryCollection(new Dictionary<string, StringValues>
+            {
+                { "filterBy", new StringValues("and(startedAt:gte(0001-01-01),startedAt:lte(2023-06-30))") }
+            }),
+            CultureInfo.CurrentCulture
+        );
+        
+        var metadataProvider = new EmptyModelMetadataProvider();
         var context = new DefaultModelBindingContext
         {
             ModelName = "filterBy",
             ModelState = new ModelStateDictionary(),
-            ModelMetadata = new DefaultModelMetadata(
-                new DefaultModelMetadataProvider(compositeMetadataDetailsProvider), 
-                compositeMetadataDetailsProvider,
-                new DefaultMetadataDetails( ModelMetadataIdentity.ForType(typeof(IFiltering<>)), ModelAttributes.GetAttributesForType(typeof(IFiltering<>)))
-            ),
-            ValueProvider = new QueryStringValueProvider(
-                new BindingSource(Guid.NewGuid().ToString(), "filterBy", false, true),
-                new QueryCollection(new Dictionary<string, StringValues>
-                {
-                    { "filterBy", new StringValues("and(startedAt:gte(0001-01-01),startedAt:lte(2023-06-30))")}
-                }),
-                CultureInfo.CurrentCulture
-            )
+            ModelMetadata = metadataProvider.GetMetadataForType(typeof(IFiltering<>)),
+            ValueProvider = valueProvider
         };
         await binder.BindModelAsync(context);
+        Assert.Multiple(() =>
+        {
+            Assert.That(context.Result.IsModelSet, Is.True);
+            Assert.That(context.Result.Model, Is.Not.Null);
+        });
     }
 }
